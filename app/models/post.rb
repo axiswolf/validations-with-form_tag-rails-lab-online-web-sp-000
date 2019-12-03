dbaf2f0d83f58f344357cf8c4c78af9e56843dc7
Karen V <karenvo@usc.edu>
2019-12-03 06:27:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with CategoryValidator
-
   validates :title, presence: true
   validates :content, :length => {:minimum => 100}
-  validate :category
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
