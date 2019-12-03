8c94fa43b51af1e044295034783ebf79a273f5fb
Karen V <karenvo@usc.edu>
2019-12-03 06:20:19 +0000

    Automatically backed up by Learn

--- app/models/category_validator.rb
+++ app/models/category_validator.rb
@@ -1,7 +1,9 @@
 class CategoryValidator < ActiveModel::Validator
   def validate(category)
     unless category.match?("Fiction" || "Non-Fiction")
-      record.errors[:category] << "Fiction or Non-Fiction Only!"
+      category.errors[:category] << "Fiction or Non-Fiction Only!"
     end
   end
+
+
 end
