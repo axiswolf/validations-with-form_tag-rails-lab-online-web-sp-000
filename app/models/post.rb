7bcbec6eb83f139b75c552edc3c3d0e79bfb9f7b
Karen V <karenvo@usc.edu>
2019-12-03 06:24:11 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   include ActiveModel::Validations
   validates_with CategoryValidator
 
-  # validates :title, presence: true
-  # validates :content, :length => {:minimum => 100}
+  validates :title, presence: true
+  validates :content, :length => {:minimum => 100}
+  validate :category
 end
